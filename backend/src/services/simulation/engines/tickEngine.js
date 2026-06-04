d0700a9609295f99eb1b6252e1d92ae05f3570c6
Sahil Raj Verma <sahilrv196@gmail.com>
2026-06-05 03:26:13 +0530

    Fix weekly simulation double increment

--- backend/src/services/simulation/engines/tickEngine.js
+++ backend/src/services/simulation/engines/tickEngine.js
@@ -3,8 +3,6 @@ import { processWritingProjects } from "./writerEngine.js";
 import { processWriterAging } from "../helpers/agingHelper.js";
 
 export const processWeeklyTick = async (gameState) => {
-  gameState.currentWeek += 1;
-
   await processWritingProjects(gameState);
 
   processWriterAging(gameState);
