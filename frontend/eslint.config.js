b715427a321193ead892e0d3b77358385763dce5
Agnasmaria <agnasmaria2006@gmail.com>
2026-07-22 22:52:12 +0530

    chore: turn off react-hooks static-components rule in eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,6 +19,7 @@ export default defineConfig([
     },
     rules: {
       'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/static-components': 'off',
       'react-hooks/rules-of-hooks': 'off',
       'no-unused-vars': 'off',
     },
